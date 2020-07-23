39ef6fc65c702b451d033533421513749def51d3
harriet-the-spy <dpcairns@gmail.com>
2020-07-23 11:31:21 -0700

    add form

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+import { makeUser } from './userUtils.js';
 // import functions and grab DOM elements
+const form = document.querySelector('form');
 
 // initialize state
 
 // set event listeners to update state and DOM
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
 
-console.log('=============================\n');
-console.log('|| home page');
-console.log('\n=============================');
+    const user = makeUser(formData);
+
+    const stringyUser = JSON.stringify(user);
+    localStorage.setItem('USER', stringyUser);
+});
+
+
